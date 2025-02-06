aa3c82965b5d2feb36ed14cb378004f0a17e9535
AJRovers <mr.halim.rahman@gmail.com>
2025-02-06 12:28:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from streamlit_shap import st_shap
 
 # Load the dataset (assuming it's in the same directory)
 # customer = pd.read_csv("Customer Churn.csv")
-# customer = pd.read_csv("Harumanis Grade.csv")
+customer = pd.read_csv("Harumanis Grade.csv")
 
 # Preprocessing
 X = customer.drop("Churn", axis=1)
@@ -32,7 +32,7 @@ explainer = shap.TreeExplainer(clf)
 shap_values = explainer.shap_values(X_test)
 
 # Streamlit app
-st.title("SHAP Analysis for Customer Churn")
+st.title("SHAP Analysis for Harumanis Grade")
 
 # Part 1: General SHAP Analysis
 st.header("Part 1: General SHAP Analysis")
@@ -52,8 +52,12 @@ st.header("Part 2: Individual Input Prediction & Explanation")
 # Input fields for features
 input_data = {}
 for feature in X.columns:
-    if feature in ['Call  Failure', 'Complains', 'Subscription  Length', 'Status' 'Seconds of Use',
-                   'Frequency of use', 'Frequency of SMS', 'Distinct Called Numbers', 'Age Group', 'Age']:
+#    if feature in ['Call  Failure', 'Complains', 'Subscription  Length', 'Status' 'Seconds of Use',
+#                   'Frequency of use', 'Frequency of SMS', 'Distinct Called Numbers', 'Age Group', 'Age']:
+#        input_data[feature] = st.number_input(f"Enter {feature}:", value=int(X_test[feature].mean()), step=1)
+#    else:  # For other features, keep the original input type
+#        input_data[feature] = st.number_input(f"Enter {feature}:", value=X_test[feature].mean())
+    if feature in ['Weight', 'Length', 'Circumference']:
         input_data[feature] = st.number_input(f"Enter {feature}:", value=int(X_test[feature].mean()), step=1)
     else:  # For other features, keep the original input type
         input_data[feature] = st.number_input(f"Enter {feature}:", value=X_test[feature].mean())
@@ -67,8 +71,10 @@ prediction = clf.predict(input_df)[0]
 probability = clf.predict_proba(input_df)[0][1]  # Probability of churn
 
 # Display prediction
-st.write(f"**Prediction:** {'Churn' if prediction == 1 else 'No Churn'}")
-st.write(f"**Churn Probability:** {probability:.2f}")
+# st.write(f"**Prediction:** {'Churn' if prediction == 1 else 'No Churn'}")
+# st.write(f"**Churn Probability:** {probability:.2f}")
+st.write(f"**Prediction:** {'Good Quality' if prediction == 1 else 'Normal Quality'}")
+st.write(f"**Quality Probability:** {probability:.2f}")
 
 # SHAP explanation for the input
 shap_values_input = explainer.shap_values(input_df)
